464f40dbe7467dcc72941d0ad4244faa1c92fffe
yourtitogwapito <122256680+yourtitogwapito@users.noreply.github.com>
2023-01-20 13:40:38 +0800

    Update hellopeople.py
    change script

--- hellopeople.py
+++ hellopeople.py
@@ -2,5 +2,5 @@ import pandas as pd
 import selenium as sl
 
 print("hello pips")
-print(pd.__version__)
-print(sl.__version__)
+print(f"pandas version: {pd.__version__}")
+print(f"selenium version: {sl.__version__}")
